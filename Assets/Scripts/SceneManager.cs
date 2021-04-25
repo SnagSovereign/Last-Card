91a46b9fc69e43e320c56333b0c166aaa226d528
SnagSovereign <joeysimon@outlook.com>
2021-04-25 16:27:59 +1000

    Deck can be generated and shuffled, Setup scene and SceneManager created.

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public void LoadScene(string scene)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+}
